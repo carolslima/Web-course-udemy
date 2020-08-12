b906062daa2046775025bdc68cdb8a974fe353fb
Caroline <carolhiprock@hotmail.com>
2020-08-12 11:26:52 -0300

    Object

---
+++ myExercises/javascript/object/OO.js
@@ -0,0 +1,22 @@
+// codigo não executável!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO (orientação ao objeto)
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento(){
+    // ...
+  }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+// principios importantes
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
